7c3e67e75923937ea66f823312b82aef429f74af
JeanaeClark <jeclark@udel.edu>
2023-05-04 20:19:01 -0400

    working with cam issues

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     let but = document.getElementById("but");
     let video = document.getElementById("vid");
     let mediaDevices = navigator.mediaDevices;
-    // let click_button = document.querySelector("#click-photo");
-    // let canvas = document.querySelector("#canvas");
     vid.muted = true;
     but.addEventListener("click", () => {
 
@@ -23,12 +21,4 @@ document.addEventListener("DOMContentLoaded", () => {
             })
             .catch(alert);
     });
-
-//     click_button.addEventListener('click', function() {
-//         canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-//         let image_data_url = canvas.toDataURL('image/jpeg');
- 
-//         // data url of the image
-//         console.log(image_data_url);
-//  });
 });
