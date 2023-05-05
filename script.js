cf6bcc2ce13fc6f566c8594daa6f8ca1ffb5ef87
JeanaeClark <jeclark@udel.edu>
2023-05-04 20:00:06 -0400

    Working on camera - running into issue with http/https

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
+document.addEventListener("DOMContentLoaded", () => {
+    let but = document.getElementById("but");
+    let video = document.getElementById("vid");
+    let mediaDevices = navigator.mediaDevices;
+    vid.muted = true;
+    but.addEventListener("click", () => {
 
-const status = document.getElementById('status');
-status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
+        // Accessing the user camera and video.
+        mediaDevices
+            .getUserMedia({
+                video: true,
+                audio: true,
+            })
+            .then((stream) => {
+
+                // Changing the source of video to current stream.
+                video.srcObject = stream;
+                video.addEventListener("loadedmetadata", () => {
+                    video.play();
+                });
+            })
+            .catch(alert);
+    });
+});
