ad14d234a931ae44c88d767acd96c0c5e02ea34c
JeanaeClark <jeclark@udel.edu>
2023-05-04 20:26:40 -0400

    Messing with camera

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let but = document.getElementById("but");
-    let video = document.getElementById("vid");
-    let mediaDevices = navigator.mediaDevices;
-    vid.muted = true;
-    but.addEventListener("click", () => {
+let camera_button = document.querySelector("#start-camera");
+let video = document.querySelector("#video");
+let click_button = document.querySelector("#click-photo");
+let canvas = document.querySelector("#canvas");
 
-        // Accessing the user camera and video.
-        mediaDevices
-            .getUserMedia({
-                video: true,
-                audio: true,
-            })
-            .then((stream) => {
+camera_button.addEventListener('click', async function() {
+   	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+	video.srcObject = stream;
+});
 
-                // Changing the source of video to current stream.
-                video.srcObject = stream;
-                video.addEventListener("loadedmetadata", () => {
-                    video.play();
-                });
-            })
-            .catch(alert);
-    });
+click_button.addEventListener('click', function() {
+   	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+   	let image_data_url = canvas.toDataURL('image/jpeg');
+
+   	// data url of the image
+   	console.log(image_data_url);
 });
